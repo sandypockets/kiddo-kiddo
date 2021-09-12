c34bdc768a5aba3b5d2786932814ff07862eeac1
sandypockets <zac@znacreative.com>
2021-09-12 15:13:05 -0400

    Initial project set up

---
+++ routes/index.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Kiddo Kiddo | Data Visualization' });
+});
+
+module.exports = router;
